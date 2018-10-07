25ac52012a3cb781538d4029d4cf840aa7d05c84
Hazem Saleh <hazems@apache.org>
2018-10-07 01:35:16 -0400

    Upgrading libraries

--- app/src/main/java/com/example/test/mvvmsampleapp/view/ui/MainActivity.java
+++ app/src/main/java/com/example/test/mvvmsampleapp/view/ui/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.test.mvvmsampleapp.view.ui;
 
-import android.arch.lifecycle.LifecycleActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.test.mvvmsampleapp.R;
 import com.example.test.mvvmsampleapp.service.model.Project;
@@ -13,7 +13,7 @@ import javax.inject.Inject;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
-public class MainActivity extends LifecycleActivity implements HasSupportFragmentInjector {
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
     @Inject
     DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
